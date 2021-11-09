20608052f872755fe2822735085ecc9b272713e5
Paulo de Freitas <paulohdef1@gmail.com>
2021-11-09 11:59:04 -0300

    implementação service likes

--- gatitobook/src/app/animais/animais.module.ts
+++ gatitobook/src/app/animais/animais.module.ts
@@ -6,13 +6,17 @@ import { AnimaisRoutingModule } from './animais-routing.module';
 import { ListaAnimaisComponent } from './lista-animais/lista-animais.component';
 import { AnimalComponent } from './animal/animal.component';
 import { GradeFotosAnimaisComponent } from './grade-fotos-animais/grade-fotos-animais.component';
+import { DetalheAnimalComponent } from './detalhe-animal/detalhe-animal.component';
+import { ComentariosComponent } from './detalhe-animal/comentarios/comentarios.component';
 
 
 @NgModule({
   declarations: [
     ListaAnimaisComponent,
     AnimalComponent,
-    GradeFotosAnimaisComponent
+    GradeFotosAnimaisComponent,
+    DetalheAnimalComponent,
+    ComentariosComponent
   ],
   imports: [
     CommonModule,
